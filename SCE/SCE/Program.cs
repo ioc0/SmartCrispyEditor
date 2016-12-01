042fca342065f79a699fef2a50e0273158e7e53b
ioc0 <ioc0kernel@gmail.com>
2016-12-01 12:19:04 +0300

    Add files via upload

---
+++ SCE/SCE/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace SCE
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+        public static string Title
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                    if (!String.IsNullOrEmpty(titleAttribute.Title))
+                        return titleAttribute.Title;
+                }
+
+                // If there was no Title attribute, or if the Title attribute was the empty string, return the .exe name
+                return Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+            }
+        }
+    }
+}
